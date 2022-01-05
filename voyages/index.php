e78deaf3b948be5e64a18ea3807d87d53ccc3c4e
j-serme <88367947+j-serme@users.noreply.github.com>
2022-01-05 09:54:33 +0100

    Add files via upload

---
+++ voyages/index.php
@@ -0,0 +1,84 @@
+<?php
+require_once "logique.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/5/yeti/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@600&display=swap" rel="stylesheet">
+    <title>Voyages Troubadour</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/HB/voyages">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto d-flex flex-direction-column justify-content-center align-items-center" style="color:white">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/HB/voyages">Voyages Troubadour</a>
+                    </li>
+                    <li class="ms-3">
+                        <?php if ($isLoggedIn == true) { ?>
+                            Bonjour <?= $_SESSION['username'] ?>
+                        <?php } ?>
+                    </li>
+                </ul>
+                <?php if ($isLoggedIn == false) { ?>
+                    <form class="d-flex" method="post">
+                        <input class="form-control me-sm-2" type="text" placeholder="Username" name="username">
+                        <input class="form-control me-sm-2" type="password" placeholder="Password" name="password">
+                        <input type="submit" value="Valider">
+                    </form>
+                <?php } else { ?>
+                    <form method="post">
+                        <input type="submit" value="déconnexion" name="logout">
+                    </form>
+                <?php } ?>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="col-12 d-flex flex-direction-column flex-wrap justify-content-center my-4">
+        <?php foreach ($voyages as $voyage) { ?>
+
+            <div style="background-image:url('images/<?= $voyage['image'] ?>')" class='destination row col-3 mx-3 my-4 flex-direction-column align-items-center justify-content-center'>
+                <div class="text-trip py-3">
+                    <h1><?= $voyage['destination'] ?> </h1>
+                    <?php if ($isLoggedIn == true) { ?>
+                        <h2 style="color:red"><?= $voyage['prix'] * 0.8 ?> € </h2>
+                    <?php } else { ?> <h2 style="color:red"><?= $voyage['prix'] ?> € </h2><?php } ?>
+
+                    <h3><?= $voyage['duree'] ?> Jours</h3>
+                    <h3>Pour <?= $voyage['personnes'] ?> voyageurs</h3>
+                    <a href="voyage.php?destination=<?= $voyage['destination'] ?>">VOIR CE VOYAGE</a>
+                </div>
+
+
+            </div>
+        <?php } ?>
+    </div>
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ voyages/logique.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+$voyages = [
+
+    [
+        "destination" => "Canada",
+        "prix" => 3455,
+        "duree" => 19,
+        "image" => "canada.jpg",
+        "personnes" => 2,
+        "transport" => "avion"
+    ],
+    [
+        "destination" => "Mexique",
+        "prix" => 2500,
+        "duree" => 12,
+        "image" => "mexique.jpg",
+        "personnes" => 3,
+        "transport" => "avion"
+    ],
+    [
+        "destination" => "Pragues",
+        "prix" => 1321,
+        "duree" => 15,
+        "image" => "pragues.jpg",
+        "personnes" => 4,
+        "transport" => "train"
+    ],
+    [
+        "destination" => "Danemark",
+        "prix" => 2672,
+        "duree" => 12,
+        "image" => "danemark.jpg",
+        "personnes" => 4,
+        "transport" => "avion"
+    ],
+    [
+        "destination" => "Italie",
+        "prix" => 854,
+        "duree" => 10,
+        "image" => "italie.jpg",
+        "personnes" => 1,
+        "transport" => "bus"
+    ],
+
+
+];
+
+$users = [
+
+    [
+        "username" => "michel",
+        "password" => "blabla"
+    ],
+    [
+        "username" => "etienne",
+        "password" => "bidule"
+    ],
+];
+
+
+$isLoggedIn = false;
+
+if (isset($_POST['logout'])) {
+    unset($_SESSION['isLoggedIn']);
+}
+
+foreach ($users as $user) {
+    if (
+        isset($_POST['username']) && ($_POST['username'] == $user['username'])
+
+        &&
+
+        (isset($_POST['password'])) && ($_POST['password'] == $user['password'])
+
+    ) {
+
+        $isLoggedIn = true;
+        $_SESSION['isLoggedIn'] = true;
+    }
+}
+
+
+if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']) {
+    $isLoggedIn = true;
+}
